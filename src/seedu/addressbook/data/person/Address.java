1cf13175412815ef5518bbf8ce81ec2c8fc8c174
Lok You Tan <e0003133@u.nus.edu>
2017-02-01 00:04:02 +0800

    Split address into 4 more classes - Block, Street, Unit, PostalCode

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,11 +8,16 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "a/BLOCK, STREET, UNIT, POSTAL_CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
     public final String value;
+    public final Block block;
+    public final Street street;
+    public final Unit unit;
+    public final PostalCode postalCode;
+    
     private boolean isPrivate;
 
     /**
@@ -21,12 +26,18 @@ public class Address {
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
-        String trimmedAddress = address.trim();
         this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
+        if (!isValidAddress(address)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        this.value = address;
+        
+        String[] addressTokens = value.split(",");
+        block = new Block(addressTokens[0]);
+        street = new Street(addressTokens[1]);
+        unit = new Unit(addressTokens[2]);
+        postalCode = new PostalCode(addressTokens[3]);
+        
     }
 
     /**
