f57652de5854fdcb37e0d854f7042f8dadfd4724
Lok You Tan <e0003133@u.nus.edu>
2017-02-11 13:00:40 +0800

    Add isSimilar method to Name Add NameTest

---
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,20 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class NameTest {
+
+    @Before
+    public void setup() throws IllegalValueException {
+        Name tester = new Name("John K Smith");
+    }
+    
+    @Test
+    public void testIsSimilar() throws IllegalValueException {
+        
+    }
+
+}
